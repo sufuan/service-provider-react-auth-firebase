07a526029b0169d15ff24731f7714e53cb338b07
sufuan <abu49539@gmail.com>
2022-04-17 10:16:14 +0600

    service added

--- src/component/Services/Services.js
+++ src/component/Services/Services.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service';
 
 const Services = () => {
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+
+    }, [])
     return (
-        <div>
-                this service
+        <div className='w-90vh px-4 pt-10 pb-24 mx-auto max-w-7xl md:px-2'>
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 justify-items-center">
+
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+
+            </div>
         </div>
     );
 };
