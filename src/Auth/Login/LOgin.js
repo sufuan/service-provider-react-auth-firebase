8751e564b0fb2dcdf5935a1d134486c6bb382c1d
sufuan <abu49539@gmail.com>
2022-04-18 19:10:34 +0600

    auth done

--- src/Auth/Login/LOgin.js
+++ src/Auth/Login/LOgin.js
@@ -69,6 +69,9 @@ const LOgin = () => {
         case 'auth/internal-error':
           setErrors({ ...errors, generaleError: 'internal error' })
           break
+          case 'auth/network-request-failed':
+          setErrors({ ...errors, generaleError: 'Network error' })
+          break
         default:
           setErrors({ ...errors, generaleError: 'something went wrong' })
           break
@@ -147,10 +150,10 @@ const LOgin = () => {
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
-                {errors?.passError && <p>{errors.passError}</p>}
+                {errors?.passError && <p className='text-red-600 text-sm'>{errors.passError}</p>}
               </div>
 
-              {errors.generaleError && <p>{errors.generaleError}</p>}
+              {errors.generaleError && <p className='text-red-600 text-sm'>{errors.generaleError}</p>}
 
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
--- src/Auth/Register/Register.js
+++ src/Auth/Register/Register.js
@@ -1,272 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
-// import { useLocation, useNavigate } from 'react-router-dom';
-// import { auth } from '../../firebase';
-// import googlelogo from '../../assets/google.png'
-
-// const Register = () => {
-
-//   const [userInfo, setUserinfo] = useState({
-//     email: '',
-//     password: '',
-//     confirmPassword: '',
-//   })
-//   const [errors, setErrors] = useState({
-//     emailError: '',
-//     passError: '',
-//     generaleError: '',
-//   })
-//   const [createUserWithEmailAndPassword, user, hookerror,] = useCreateUserWithEmailAndPassword(auth);
-//   const [signInWithGoogle, googleUser] = useSignInWithGoogle(auth);
-
-
-//   const handleEmail = e => {
-
-//     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value)) {
-//       setUserinfo({ ...userInfo, email: e.target.value })
-//       setErrors({ ...errors, emailError: '' })
-
-//     }
-//     else {
-//       setErrors({ ...errors, emailError: 'invalid email' })
-//       setUserinfo({ ...userInfo, email: '' })
-//     }
-
-//   }
-//   const handlePassword = e => {
-//     if (/.{6,}/.test(e.target.value)) {
-//       setUserinfo({ ...userInfo, password: e.target.value })
-//       setErrors({ ...errors, passError: '' })
-//     }
-//     else {
-//       // error 
-//       setErrors({ ...errors, passError: 'password should be 6 char' })
-//       setUserinfo({ ...userInfo, password: '' })
-//     }
-
-//   }
-
-
-//   const handleConfirmPassword = e => {
-//     if (e.target.value === userInfo.password) {
-//       setUserinfo({ ...userInfo, confirmPassword: e.target.value })
-//       setErrors({ ...errors, passError: '' })
-//     }
-//     else {
-//       // error 
-//       setErrors({ ...errors, passError: 'password dont match' })
-//       setUserinfo({ ...userInfo, confirmPassword: '' })
-//     }
-
-//   }
-
-
-//   const handleLogin = (e) => {
-//     e.preventDefault()
-//     createUserWithEmailAndPassword(userInfo.email, userInfo.password)
-//   }
-
-//   useEffect(() => {
-
-//     if (hookerror) {
-//       switch (hookerror.code) {
-//         case 'auth/invalid-email':
-//           setErrors({ ...errors, generaleError: 'invalid-email' })
-//           break
-//         case 'auth/invalid-password':
-//           setErrors({ ...errors, generaleError: 'invalid-password' })
-//           break
-//         case 'auth/user-not-found':
-//           setErrors({ ...errors, generaleError: 'user not found' })
-//           break
-//         default:
-//           setErrors({ ...errors, generaleError: 'something went wrong' })
-//           break
-//       }
-
-
-
-//       console.log(hookerror)
-//       // setErrors({...errors, generaleError:'something went wrong'})
-//     }
-//   }, [hookerror])
-
-
-//   const location = useLocation()
-//   const navigate = useNavigate()
-//   const from = location?.state?.from?.pathname || '/'
-
-
-//   useEffect(() => {
-//     if (user) {
-//       navigate(from)
-//     }
-//   }, [user])
-
-//   useEffect(() => {
-//     if (googleUser) {
-//       navigate(from)
-//     }
-//   }, [googleUser])
-
-
-//   return (
-//     <>
-
-//       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-//         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-
-//           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up to your account</h2>
-//           <p className="mt-2 text-center text-sm text-gray-600">
-            
-
-//           </p>
-//         </div>
-
-//         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-//           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-//             <form className="space-y-6" onSubmit={handleLogin}>
-//               <div>
-//                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-//                   Email address
-//                 </label>
-//                 <div className="mt-1">
-//                   <input
-//                     onChange={handleEmail}
-//                     id="email"
-//                     name="email"
-//                     type="email"
-//                     autoComplete="email"
-//                     required
-//                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                   />
-//                 </div>
-//                 {errors?.emailError && <p className='text-red-600'>{errors.emailError}</p>}
-//               </div>
-
-//               <div>
-//                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-//                   Password
-//                 </label>
-//                 <div className="mt-1">
-//                   <input
-//                     onChange={handlePassword}
-//                     id="password"
-//                     name="password"
-//                     type="password"
-//                     autoComplete="current-password"
-//                     required
-//                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                   />
-//                 </div>
-//                 {errors?.passError && <p>{errors.passError}</p>}
-//               </div>
-
-//               <div>
-//                 <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
-//                   Confirm Password
-//                 </label>
-//                 <div className="mt-1">
-//                   <input
-//                     onChange={handleConfirmPassword}
-//                     id="confirmPassword"
-//                     name="confirmPassword"
-//                     type="password"
-//                     autoComplete="current-password"
-//                     required
-//                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                   />
-//                 </div>
-//                 {errors?.passError && <p>{errors.passError}</p>}
-//               </div>
-
-//               {errors.generaleError && <p>{errors.generaleError}</p>}
-
-//               <div className="flex items-center justify-between">
-//                 <div className="flex items-center">
-//                   <input
-//                     id="remember-me"
-//                     name="remember-me"
-//                     type="checkbox"
-//                     className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-//                   />
-//                   <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-//                     Remember me
-//                   </label>
-//                 </div>
-
-
-//               </div>
-
-//               <div>
-//                 <button
-//                   type="submit"
-//                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-//                 >
-//                   Sign up
-//                 </button>
-//               </div>
-
-
-//             </form>
-
-//             <div className="mt-6">
-//               <div className="relative">
-//                 <div className="absolute inset-0 flex items-center">
-//                   <div className="w-full border-t border-gray-300" />
-//                 </div>
-//                 <div className="relative flex justify-center text-sm">
-//                   <span className="px-2 bg-white text-gray-500">Or continue with</span>
-//                 </div>
-//               </div>
-
-//               <div className="mt-6 grid grid-cols-3 gap-3">
-//                 <div>
-//                   <button onClick={() => signInWithGoogle()}
-                   
-//                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-//                   >
-//                     <span className="sr-only">Sign in with Google</span>
-//                    <img className='w-50 h-5' src={googlelogo} alt="" />
-//                   </button>
-//                 </div>
-
-//                 <div>
-//                   <button  disabled
-
-//                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-//                   >
-//                     <span className="sr-only">Sign in with Gitbub</span>
-//                     <h1>Github</h1>
-//                   </button>
-//                 </div>
-
-//                 <div>
-//                   <button disabled
-
-//                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-//                   >
-//                     <span className="sr-only">Sign in with facebook</span>
-//                     <h1>facebook</h1>
-//                   </button>
-//                 </div>
-
-
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Register;
-
-
-
 import React, { useEffect, useState } from 'react';
-import { useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase';
 import googlelogo from '../../assets/google.png'
@@ -277,7 +10,7 @@ const Register = () => {
   const [userInfo, setUserinfo] = useState({
     email: '',
     password: '',
-    confirmPassword:'',
+    confirmPassword: '',
   })
   const [errors, setErrors] = useState({
     emailError: '',
@@ -292,7 +25,7 @@ const Register = () => {
     user,
     loading,
     hookerror,
-  ] = useCreateUserWithEmailAndPassword(auth);
+  ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification:true});
 
   const [signInWithGoogle, googleUser] = useSignInWithGoogle(auth);
 
@@ -316,20 +49,20 @@ const Register = () => {
     }
     else {
       // error 
-      setErrors({...errors, passError:'password should be 6 char'})
+      setErrors({ ...errors, passError: 'password should be 6 char' })
       setUserinfo({ ...userInfo, password: '' })
     }
 
   }
 
   const handleConfirmPassword = e => {
-    if (e.target.value===userInfo.password) {
+    if (e.target.value === userInfo.password) {
       setUserinfo({ ...userInfo, confirmPassword: e.target.value })
       setErrors({ ...errors, passError: '' })
     }
     else {
       // error 
-      setErrors({...errors, passError:'password dont match'})
+      setErrors({ ...errors, passError: 'password dont match' })
       setUserinfo({ ...userInfo, confirmPassword: '' })
     }
 
@@ -353,13 +86,13 @@ const Register = () => {
         case 'auth/email-already-in-use':
           setErrors({ ...errors, generaleError: 'email is already registered' })
           break
-       
+
         case 'auth/internal-error':
           setErrors({ ...errors, generaleError: 'internal error' })
           break
-          case 'auth/network-request-failed':
-            setErrors({ ...errors, generaleError: 'Network error' })
-            break
+        case 'auth/network-request-failed':
+          setErrors({ ...errors, generaleError: 'Network error' })
+          break
         default:
           setErrors({ ...errors, generaleError: 'something went wrong' })
           break
@@ -422,7 +155,7 @@ const Register = () => {
                 </div>
                 {errors?.emailError && <p className='text-red-600'>{errors.emailError}</p>}
               </div>
-                 {/* input password  */}
+              {/* input password  */}
               <div>
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                   Password
@@ -430,41 +163,41 @@ const Register = () => {
                 <div className="mt-1">
                   <input
                     onChange={handlePassword}
-                   
+
                     type="password"
                     autoComplete="current-password"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
-                {errors?.passError && <p>{errors.passError}</p>}
+                {errors?.passError && <p className='text-red-600 text-sm'>{errors.passError}</p>}
               </div>
-              
-                   {/* confim password  */}
 
-                   <div>
+              {/* confim password  */}
+
+              <div>
                 <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
-                Confim Password
+                  Confim Password
                 </label>
                 <div className="mt-1">
                   <input
                     onChange={handleConfirmPassword}
-                   
+
                     type="password"
                     autoComplete="current-password"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
-              
-              </div>    
+
+              </div>
               {/* {errors?.passError && <p>{errors.passError}</p>} */}
 
               {errors.generaleError && <p>{errors.generaleError}</p>}
 
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
-                 <Link className="font-medium text-indigo-600 hover:text-indigo-500" to="/login">Login now</Link>
+                  <Link className="font-medium text-indigo-600 hover:text-indigo-500" to="/login">Login now</Link>
                 </div>
 
                 <div className="text-sm">
@@ -500,12 +233,12 @@ const Register = () => {
 
                 {/* google login  */}
                 <div>
-                  <button  onClick={() => signInWithGoogle()}
+                  <button onClick={() => signInWithGoogle()}
 
                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                   >
                     <span className="sr-only">Sign in with Google</span>
-                    <img  className='w-50 h-5' src={googlelogo} alt="" />
+                    <img className='w-50 h-5' src={googlelogo} alt="" />
                   </button>
                 </div>
 
@@ -525,7 +258,7 @@ const Register = () => {
 
                 <div>
                   <button disabled
-                 
+
                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                   >
                     <span className="sr-only">Sign in with GitHub</span>
